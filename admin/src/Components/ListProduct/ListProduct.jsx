2d93f5d8353203488dad2f9f3af77a1f8475a65f
SamyakSigdel <samyaksigdel77@gmail.com>
2024-05-26 19:08:10 +0545

    user schema and signup

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,8 +6,8 @@ export const ListProduct = () => {
 
   const [allproducts,setAllProducts] = useState([]);
 
-  const fetchInfo =async ()=> {
-    await fetch('http://localhost:4000/allproducts')
+  const fetchInfo = async ()=>{
+    await fetch('http://localhost:4000/allproduct')
     .then((res)=>res.json())
     .then((data)=>{setAllProducts(data)});
   }
@@ -16,10 +16,22 @@ export const ListProduct = () => {
     fetchInfo();
   },[])
 
+const remove_product = async (id)=>{
+  await fetch('http://localhost:4000/removeproduct',{
+  method:'POST',
+  headers:{
+    Accept:'application/json',
+    'Content-Type':'application/json',
+  },
+  body:JSON.stringify({id:id})
+})
+await fetchInfo();
+}
+
   return (
     <div className='list-product'>
       <h1>All Products List</h1>
-      <div className="listproduct_format_main">
+      <div className="listproduct-format-main">
         <p>Products</p>
         <p>Title</p>
         <p>Old Price</p>
@@ -31,21 +43,19 @@ export const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index)=>{
-          return <div key={index} className="listproduct-format-main listproduct-format" >
-              <img src={product.image} alt="" className="listproduct-producticon" />
+          return <>
+          <div key={index} className="listproduct-format-main listproduct-format" >
+              <img src={product.image} alt="" className="listproduct-product-icon" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img src={cross_icon} alt="" className="listproduct-remove-icon" />
-
+              <img src={cross_icon}  className="listproduct-remove-icon" onClick={() =>{remove_product(product.id)} } />
           </div>
+          <hr />
+          </>
         })}
-
       </div>
-
-      
-      
-       </div>
+    </div>
   )
 }
--- backend/index.js
+++ backend/index.js
@@ -133,6 +133,71 @@ app.get('/allproduct', async(req,res)=>{
 })
 
 
+//Schema creating for user model 
+
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+})
+
+// Creating endpoint for registering the user
+
+app.post('/signup',async (req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false,errors:'existing user found with same email address'})
+    }
+    let cart ={};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+
+        user:{
+            id:user.id
+        }
+    }
+
+    const token =jwt.sign(data,'secret_ecom')
+    res.json({success:true,token})
+
+
+
+})
+
+
+
+
+
+
+
+
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on port "+ port);
